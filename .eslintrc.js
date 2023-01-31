9ab3b2c8dd17b37638bc4635c6c8416532c73677
Ziggy Chen <ziggycs9796@hotmail.com>
2023-01-31 23:16:37 +0800

    feat: project init

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,23 @@
-module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    "plugin:vue/vue3-essential",
-    "standard-with-typescript",
-    "plugin:prettier/recommended",
-  ],
-  overrides: [],
-  parserOptions: {
-    // parser: "@typescript-eslint/parser",
-    ecmaVersion: "latest",
-    // project: ["./tsconfig.json"],
-    sourceType: "module",
-  },
-  plugins: ["vue"],
-  rules: {},
-};
+// module.exports = {
+//   env: {
+//     browser: true,
+//     es2021: true,
+//     node: true,
+//   },
+//   extends: [
+//     "plugin:vue/vue3-essential",
+//     "standard-with-typescript",
+//     "plugin:prettier/recommended",
+//     "eslint-config-prettier",
+//   ],
+//   overrides: [],
+//   parserOptions: {
+//     ecmaVersion: "latest",
+//     parser: "@babel/eslint-parser",
+//     project: ["tsconfig.json"],
+
+//     sourceType: "module",
+//   },
+//   plugins: ["vue"],
+//   rules: {},
+// };
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import eslintPlugin from "vite-plugin-eslint"; // 导入包
+import eslint from "vite-plugin-eslint"; // 导入包
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    eslintPlugin({
+    eslint({
       include: ["src/**/*.ts", "src/*.ts", "src/**/*.vue", "src/*.vue"],
     }),
   ],
