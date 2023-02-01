c7f8f0eba15257e27ec6072dd3de19265897a533
Ziggy Chen <ziggycs9796@hotmail.com>
2023-02-01 13:50:28 +0800

    feat: project init

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,9 +1,13 @@
+<script lang="ts">
+const b = "xx";
+</script>
+
 <script setup lang="ts">
 import { ref } from "vue";
 
-defineProps<{ msg: string }>();
-
-const count = ref(0);
+let props = defineProps<{ msg: string }>();
+let count = ref(0);
+count.value = 1;
 const a = "xixi";
 </script>
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,24 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import eslint from "vite-plugin-eslint"; // 导入包
+import checker from "vite-plugin-checker"; // 导入包
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    eslint({
-      include: ["src/**/*.ts", "src/*.ts", "src/**/*.vue", "src/*.vue"],
+    checker({
+      eslint: {
+        lintCommand: 'eslint "./src/**/*.{vue,js,ts}"',
+      },
+      vueTsc: true,
     }),
   ],
+  resolve: {
+    alias: [
+      {
+        find: "@",
+        replacement: "./src",
+      },
+    ],
+  },
 });
