c77a6b842492bd30a8f806b7b9d8d19236d16ac7
Ziggy Chen <ziggycs9796@hotmail.com>
2023-02-15 16:09:19 +0800

    chore: eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,26 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    node: true
-  },
-  extends: [
-    "eslint:recommended",
-    "plugin:vue/vue3-essential",
-    "plugin:@typescript-eslint/recommended",
-    "eslint-config-prettier"
-  ],
-  overrides: [],
-  parser: "vue-eslint-parser",
-  parserOptions: {
-    parser: "@typescript-eslint/parser",
-    ecmaVersion: "latest",
-    sourceType: "module"
-  },
-  plugins: ["vue", "@typescript-eslint"],
-  rules: {
-    indent: ["error", "tab"],
-    "linebreak-style": ["error", "unix"],
-    quotes: ["error", "double"],
-    semi: ["error", "never"]
-  }
+	env: {
+		browser: true,
+		es2021: true,
+		node: true
+	},
+	extends: [
+		"eslint:recommended",
+		"plugin:vue/vue3-essential",
+		"plugin:@typescript-eslint/recommended",
+		"eslint-config-prettier"
+	],
+	overrides: [],
+	parser: "vue-eslint-parser",
+	parserOptions: {
+		parser: "@typescript-eslint/parser",
+		ecmaVersion: "latest",
+		sourceType: "module"
+	},
+	plugins: ["vue", "@typescript-eslint"],
+	rules: {
+		indent: ["error", "tab"],
+		quotes: ["error", "double"],
+		semi: ["error", "never"]
+	}
 }
